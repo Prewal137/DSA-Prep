e5e2ae607cc88ed9beedcac58931dd82a9564062
Prewal <prewalfernandes.gorigudda@gmail.com>
2026-06-16 19:12:07 +0530

    Added new problem

---
+++ Binary_Search[1D_2D_Arrays_Search_Space]/8.cpp
@@ -0,0 +1,43 @@
+// Search element in rotated sorted array-2 (unique elements)
+//Binary search
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements in array: ";
+    cin>>n;
+    vector<int>a(n);
+    cout<<"Enter the array elements in sorted order: ";
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    int target;
+    cout<<"Enter the target to be found: ";
+    cin>>target;
+    int low=0;
+    int high=n-1;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(a[mid]==target)
+        {
+            cout<<"Target found in index "<<mid<<endl;
+        }
+        if(a[low]<=a[mid])
+        {
+            if(a[low]<=target && target<=a[mid])
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        else
+        {
+            if(a[mid]<=target && target<=a[high])
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+    }
+    return 0;
+}
