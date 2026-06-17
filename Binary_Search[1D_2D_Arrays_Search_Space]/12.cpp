4b5f00abf2a209ace25c7a2f1abd243ecf5c9c47
Prewal <prewalfernandes.gorigudda@gmail.com>
2026-06-17 20:44:59 +0530

    Added new problem

---
+++ Binary_Search[1D_2D_Arrays_Search_Space]/12.cpp
@@ -0,0 +1,33 @@
+//Single Element in sorted array
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements in array: ";
+    cin>>n;
+    vector<int>a(n);
+    cout<<"Enter the array elements in sorted order: ";
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    if(n==1)
+        cout<<"Single element is "<<a[0]<<endl;
+    if(a[0]!=a[1])
+        cout<<"Single element is "<<a[0]<<endl;
+    if(a[n-1]!=a[n-2])
+        cout<<"Single element is "<<a[n-1]<<endl;
+    int low=1;
+    int high=n-2;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(a[mid]!=a[mid-1] && a[mid]!=a[mid+1])
+            cout<<"Single element is "<<a[mid]<<endl;
+        if((mid%2==1 && a[mid]==a[mid-1])||(mid%2==0 && a[mid]==a[mid+1]))
+            low=mid+1;
+        else
+            high=mid-1;
+    }
+    return 0;
+}
