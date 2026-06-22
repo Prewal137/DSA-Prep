17350f253fcd9ca47200e00c4d200268b6177b74
Prewal <prewalfernandes.gorigudda@gmail.com>
2026-06-22 16:10:01 +0530

    Added new problem

--- Binary_Search[1D_2D_Arrays_Search_Space]/31.cpp
+++ Binary_Search[1D_2D_Arrays_Search_Space]/31.cpp
@@ -1,4 +1,4 @@
-// Peak element in 2D matrix
+//Median of row-wise sorted 2D matrix
 #include<bits/stdc++.h>
 using namespace std;
 int maxElement(vector<vector<int>>&a,int n,int m,int mid)
@@ -31,23 +31,25 @@ int main()
             cin>>a[i][j];
         }
     }
-    int low=0;
-    int high=m-1;
+    int low=a[0][0];
+    int high=a[0][m-1];
+    for(int i=1;i<n;i++)
+    {
+        low=min(low,a[i][0]);
+        high=max(high,a[i][m-1]);
+    }
+    int req=(n*m+1)/2;
     while(low<=high)
     {
         int mid=(low+high)/2;
-        int row=maxElement(a,n,m,mid);
-        int left=mid-1>=0?a[row][mid-1]:-1;
-        int right=mid+1<m?a[row][mid+1]:-1;
-        if(a[row][mid]>left && a[row][mid]>right)
-        {
-            cout<<"Peak element is : "<<a[row][mid];
-            break;
-        }
-        else if(a[row][mid]<left)
-            high=mid-1;
-        else
+        int count=0;
+        for(int i=0;i<n;i++)
+            count += upper_bound(a[i].begin(), a[i].end(), mid) - a[i].begin();
+        if(count<=req)
             low=mid+1;
+        else
+            high=mid-1;
     }
+    cout<<"Median of row-wise sorted 2D matrix "<< low;
     return 0;
 }
