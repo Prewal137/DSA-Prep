59cb34a0fb68fb499ef46ab4bab95a5013faca97
Prewal <prewalfernandes.gorigudda@gmail.com>
2026-07-01 22:59:54 +0530

    Added new problem

---
+++ Linked_List/7.cpp
@@ -0,0 +1,57 @@
+//Find the starting point of the loop in a linked list
+#include<bits/stdc++.h>
+using namespace std;
+class nodes
+{
+public:
+    int data;
+    nodes* next;
+    nodes(int data1, nodes* next1)
+    {
+        data = data1;
+        next = next1;
+    }
+    nodes(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+nodes* convertArr2LL(vector<int>& arr)
+{
+    nodes* head = new nodes(arr[0]);
+    nodes* mover = head;
+    for(int i = 1; i < arr.size(); i++)
+    {
+        nodes* temp = new nodes(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+    return head;
+}
+int main()
+{
+    vector<int> arr = {2,3,6,7,8};
+    nodes* head = convertArr2LL(arr);
+    // Create a loop
+    head->next->next->next->next->next = head->next;
+    nodes* slow = head;
+    nodes* fast = head;
+    while(fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast)
+        {
+            slow = head;
+            while(slow != fast)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            cout << "Starting point of loop is : " << slow->data << endl;
+            break;
+        }
+    }
+    return 0;
+}
